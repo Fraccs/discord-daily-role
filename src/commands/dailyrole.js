803f03b85ca24fed6d57788d6bd85c5efb6a7c1f
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-05-07 19:23:04 +0200

    Failure reply and model saving in db

--- src/commands/dailyrole.js
+++ src/commands/dailyrole.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 const { checkTimeout } = require('../util/checkNewDay');
 const { giveRole } = require('../util/giveRole');
+const GuildsSchema = require('../models/GuildsSchema');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,19 +16,53 @@ module.exports = {
     ),
     async execute(interaction, client) {
         const roleID = interaction.options.get('roleid').value;
-        
-        const embed = new MessageEmbed()
-            .setColor('#00FF00')
-            .setTitle(':white_check_mark: Role successfully set!')
-            .setTimestamp();
 
-        interaction.reply({
-            embeds: [embed]
-        });
+        GuildsSchema.findOne({ guild_id: interaction.guild.id }, (err, guild) => {
+            if(err) return console.error(err);
 
-        /* Calls giverole if new day */
-        checkTimeout(() => {
-            giveRole(client, roleID);
+            if(!guild) { // Current guild doesn't have any record
+                guild = new GuildsSchema({
+                    guild_id: interaction.guild.id,
+                    role_id: roleID
+                })
+
+                /* ---- Replying with success ---- */
+                const embed = new MessageEmbed()
+                .setColor('#00FF00')
+                .setTitle(':white_check_mark: Role successfully set!')
+                .setDescription(`${interaction.guild.roles.resolve(roleID)}`)
+                .setTimestamp();
+
+                interaction.reply({
+                    embeds: [embed]
+                });
+
+                /* Calls giverole if new day */
+                checkTimeout(() => {
+                    giveRole(client, roleID);
+                });
+            }
+            else { // Found record for the current guild
+                /* ---- Replying with failure ---- */
+                const embed = new MessageEmbed()
+                .setColor('#FF0000')
+                .setTitle(':x: Failed to set role!')
+                .setDescription('Found another record for this guild.')
+                .setTimestamp();
+
+                interaction.reply({
+                    embeds: [embed]
+                });
+
+                /* Calls giverole if new day */
+                checkTimeout(() => {
+                    giveRole(client, roleID);
+                });
+            }
+
+            guild.save((err) => {
+                if(err) return console.error(err);
+            });
         });
     }
 }
