fd937bc14b03c557b895d90c3cf8c988e0ca53db
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-05-07 19:00:07 +0200

    Fixed db connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 class Database {
     constructor() {
         this.connection = null;
     }
 
     connect() {
-        mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }).then(() => {
+        mongoose.connect(process.env.DB_URL).then(() => {
             this.connection = mongoose.connection;
         }).catch((err) => {
             console.error(err);
