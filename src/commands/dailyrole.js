7379289646ee35d70410ec6efe78117242b05cfe
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-05-08 09:30:42 +0200

    Overriding old roles (if guild already had one)

--- src/commands/dailyrole.js
+++ src/commands/dailyrole.js
@@ -40,40 +40,26 @@ module.exports = {
                     guild_id: interaction.guild.id,
                     role_id: roleID
                 })
-
-                /* ---- Replying with success ---- */
-                const embed = new MessageEmbed()
-                .setColor('#00FF00')
-                .setTitle(':white_check_mark: Role successfully set!')
-                .setDescription(`${interaction.guild.roles.resolve(roleID)}`)
-                .setTimestamp();
-
-                interaction.reply({
-                    embeds: [embed]
-                });
-
-                /* Calls giverole if new day */
-                checkTimeout(() => {
-                    giveRole(client, roleID);
-                });
             }
-            else { // Found record for the current guild
-                /* ---- Replying with failure ---- */
-                const embed = new MessageEmbed()
-                .setColor('#FF0000')
-                .setTitle(':x: Failed to set role!')
-                .setDescription('Found another record for this guild.')
-                .setTimestamp();
-
-                interaction.reply({
-                    embeds: [embed]
-                });
-
-                /* Calls giverole if new day */
-                checkTimeout(() => {
-                    giveRole(client, roleID);
-                });
+            else { // Overriding the old record roleID
+                guild.role_id = roleID;
             }
+
+            /* ---- Replying with success ---- */
+            const embed = new MessageEmbed()
+            .setColor('#00FF00')
+            .setTitle(':white_check_mark: Role successfully set!')
+            .setDescription(`${interaction.guild.roles.resolve(roleID)}`)
+            .setTimestamp();
+
+            interaction.reply({
+                embeds: [embed]
+            });
+
+            /* Calls giverole if new day */
+            checkTimeout(() => {
+                giveRole(client, roleID);
+            });
 
             guild.save((err) => {
                 if(err) return console.error(err);
