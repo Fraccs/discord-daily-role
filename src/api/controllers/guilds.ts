b9743830c0c839f6e56edd0496c2dc36da2a50af
Aliprandi Francesco <aliprandifrancescpp@gmail.com>
2022-09-10 09:35:09 +0200

    feat(controllers): create base guilds controller

---
+++ src/api/controllers/guilds.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const route = express.Router()
+
+route.get('/', async (req, res) => {
+  
+})
+
+route.post('/', async (req, res) => {
+  
+})
+
+export default route
