79c04a6fe7d983943399f48aa9afcf359a6ee8b1
Aliprandi Francesco <aliprandifrancescpp@gmail.com>
2022-09-10 09:34:37 +0200

    feat(api): create express app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
 import Database from './db/database'
+import guildRoute from './controllers/guilds'
 
+dotenv.config()
+
+const app = express()
 const db = new Database()
 
 db.connect()
+
+app.use(express.json())
+app.use('/api/guilds', guildRoute)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Started server on port: ${process.env.PORT}`)
+})
