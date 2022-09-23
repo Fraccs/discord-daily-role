05fc8f3a80a65bbd461ee5efb5914dfacf31ebce
Aliprandi Francesco <aliprandifrancescpp@gmail.com>
2022-09-23 17:33:18 +0200

    feat(controllers): add post method to guilds

--- src/api/controllers/guilds.ts
+++ src/api/controllers/guilds.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import Guild from '../models/guild'
 
 const route = express.Router()
 
@@ -7,7 +8,31 @@ route.get('/', async (req, res) => {
 })
 
 route.post('/', async (req, res) => {
-  
+  const {
+    channel_id,
+    guild_id,
+    role_id
+  } = req.body
+
+  const foundGuild = await Guild.findOne({
+    guild_id: guild_id
+  })
+
+  if(foundGuild) {
+    return res.status(400).json({
+      error: 'Guild already exists'
+    })
+  }
+
+  const newGuild = new Guild({
+    channel_id: channel_id,
+    guild_id: guild_id,
+    role_id: role_id
+  })
+
+  const savedGuild = await newGuild.save()
+
+  return res.status(200).json(savedGuild)
 })
 
 export default route
