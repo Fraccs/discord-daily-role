12942c9cac17a27b7308d4fec9c560f156b66806
Aliprandi Francesco <aliprandifrancescpp@gmail.com>
2022-09-28 13:27:35 +0200

    build(*): add host env var

--- bot/src/utils/config.js
+++ bot/src/utils/config.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const GUILD_ID = process.env.GUILD_ID
+const HOST = process.env.HOST
 const STATUS = process.env.STATUS
 const TOKEN = process.env.TOKEN
 
 const config = {
   GUILD_ID,
+  HOST,
   STATUS,
   TOKEN
 }
