f53a5cd4353993f4fa3c170bb1b2bab32d298c8f
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-09-28 17:10:36 +0200

    feat(events): log interactions

--- bot/src/events/interactionCreate.js
+++ bot/src/events/interactionCreate.js
@@ -1,7 +1,22 @@
 import handleCommand from '../utils/handleCommand.js'
+import logger from '../utils/logger.js'
 
 const interactionCreate = (client) => {
   client.on('interactionCreate', async (interaction) => {
+    const { username, discriminator } = interaction.user
+    const { id, name } = interaction.member.guild
+    const { commandName } = interaction
+    const { _hoistedOptions } = interaction.options
+
+    logger.info({
+      date: new Date(),
+      user: `${username}#${discriminator}`,
+      guildId: id,
+      guildName: name,
+      commandName,
+      _hoistedOptions
+    })
+
     if(interaction.isCommand()) {
       handleCommand(client, interaction)
     }
