0c7991d85ed20f83eebaf3f4c232eb782ca4ea05
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-09-28 22:58:05 +0200

    feat: call giverole once a day

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,7 +1,9 @@
 import { Client } from 'discord.js'
+import nodecron from 'node-cron'
 import interactionCreate from './events/interactionCreate.js'
 import ready from './events/ready.js'
 import startBot from './utils/start.js'
+import giveRole from './utils/giveRole.js'
 
 const client = new Client({
   intents: [
@@ -18,4 +20,9 @@ interactionCreate(client)
 
 client.on('ready', () => {
   ready(client)
+
+  // Call giveRole each day
+  nodecron.schedule('0 1 0 * *', () => {
+    giveRole(client)
+  })
 })
