5d7b9a13735bf1e5e942021f30832e5e8cd6b36c
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-09-28 21:25:30 +0200

    feat(commands): create show command

---
+++ bot/src/commands/show.js
@@ -0,0 +1,38 @@
+import { EmbedBuilder } from 'discord.js'
+import { SlashCommandBuilder } from '@discordjs/builders'
+import guildsService from '../services/guilds.js'
+
+const show = {
+  data: new SlashCommandBuilder()
+    .setName('show')
+    .setDescription('Show information about the current role.'),
+  run: async (client, interaction) => {
+    const guildId = interaction.guild.id
+
+    try {
+      const guild = await guildsService.getCurrent(guildId)
+
+      const embed = new EmbedBuilder()
+        .setColor('#0000FF')
+        .setTitle('🌐 Config information.')
+        .setTimestamp()
+        .setDescription(`Current role: ${interaction.guild.roles.resolve(guild.role_id)}`)
+
+      interaction.reply({
+        embeds: [embed]
+      })
+    }
+    catch(e) {
+      const embed = new EmbedBuilder()
+        .setColor('#FF0000')
+        .setTitle(':x: No information found!')
+        .setTimestamp()
+
+      interaction.reply({
+        embeds: [embed]
+      })
+    }
+  }
+}
+
+export default show
