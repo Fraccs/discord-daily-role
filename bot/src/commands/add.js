5b59cc59c30596a5cd9c12b2ed1396a862c7fa3c
Francesco Aliprandi <aliprandifrancescopp@gmail.com>
2022-09-29 21:17:43 +0200

    feat(commands): add optional output channel option

--- bot/src/commands/add.js
+++ bot/src/commands/add.js
@@ -12,9 +12,15 @@ const add = {
         .setName('role')
         .setDescription('The role that the BOT will assign to a random user.')
         .setRequired(true)
+    )
+    .addChannelOption(option =>
+      option
+        .setName('channel')
+        .setDescription('The channel where the greeting message will be printed.')
+        .setRequired(false)
     ),
   run: async (client, interaction) => {
-    const channel = interaction.channel
+    const channel = interaction.options.get('channel') ? interaction.options.get('channel').value : interaction.channel.id
     const roleId = interaction.options.get('role').value
 
     if(!isAdmin(interaction.member)) {
@@ -46,7 +52,7 @@ const add = {
 
     try {
       await guildsService.create({
-        channel_id: channel.id,
+        channel_id: channel,
         guild_id: interaction.guild.id,
         role_id: roleId
       })
@@ -54,7 +60,7 @@ const add = {
       const embed = new EmbedBuilder()
         .setColor('#00FF00')
         .setTitle(':white_check_mark: Role successfully set!')
-        .setDescription(`${interaction.guild.roles.resolve(roleId)}`)
+        .setDescription(`${interaction.guild.roles.resolve(roleId)}\n${interaction.guild.channels.resolve(channel)}`)
         .setTimestamp()
 
       interaction.reply({
